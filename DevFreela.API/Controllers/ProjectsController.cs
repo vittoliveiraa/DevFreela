fb227f2ec25043aa832cc55683167c2f23235ff8
vitto <vitto@ITO>
2023-06-19 21:01:14 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet("ObterTodos")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+
+    }
+}
---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("ObterTodos")]
+        public IActionResult Get()
+        {
+            
+            return Ok();
+        }
+        [HttpGet("ObterPorId/{id}")]
+        public IActionResult GetById(int id)
+        {
+
+            return Ok();
+        }
+        [HttpPost("CriarUsuario")]
+        public IActionResult Post() //Montar o [FromBody]
+        {
+          //  return CreatedAtAction();
+        }
+        [HttpPut("AtualizarUsuario/{id}")]
+        public IActionResult Put(int id) //Colocar o que será atualizado 
+        { 
+            return NoContent();    
+        }
+        [HttpDelete("RemoverUsuario/{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
