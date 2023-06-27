94bca61b4551b22c8fc311ca814abb7a4edb430b
Victor Oliveira <vittoliveiraa@live.com>
2023-06-27 20:11:30 -0300

    Adicionado referências no csproj

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
 
 namespace DevFreela.Application.Services.Implementations
 {
-    internal class ProjectService
+    public class ProjectService : IProjectService
     {
+        public int Create(NewProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateComment(CreateCommentInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Finish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProjectViewModel> GetAll(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjectDetailsViewModel GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UpdateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
